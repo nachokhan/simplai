1a57ae3c15efedd9e90b83b0e8ae59e7bcef144c
nachokhan <ignacio.rigoni@gmail.com>
2023-12-06 11:27:14 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='somopai',
+    version='0.1.0',
+    description='SimoPAI: Simplify OpenAI API integration with Python.',
+    author='Nachokhan',
+    author_email='ignacio.rigoni@gmail.com',
+    packages=['simoPAI'],
+    install_requires=[
+        # Dependencias
+    ],
+)
