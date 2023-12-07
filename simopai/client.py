1297d433e1651edd8b4a209947bdc7c8af1a2b9a
nachokhan <ignacio.rigoni@gmail.com>
2023-12-07 11:56:00 -0300

    added openai client connection

--- simopai/client.py
+++ simopai/client.py
@@ -1,6 +1,18 @@
 from openai import OpenAI
+from exceptions import OpenAIKeyException
 
 
 class Client():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, openai_key) -> None:
+        self.client = None
+
+        self._create_open_ai_client(openai_key)
+
+    def _create_open_ai_client(self, key) -> None:
+        if not key:
+            raise OpenAIKeyException(message="No OpenAI key provided")
+
+        if not isinstance(key, str):
+            raise OpenAIKeyException(message="OpenAI key must be a sting")
+
+        self.client = OpenAI(api_key=key)
---
+++ simopai/exceptions/OpenAIKeyException.py
@@ -0,0 +1,3 @@
+class OpenAIKeyException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
