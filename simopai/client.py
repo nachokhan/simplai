e0f60dcd9bd464e446c13c0c64c19d851cf2e909
nachokhan <ignacio.rigoni@gmail.com>
2023-12-07 15:25:37 -0300

    removed exception

--- simopai/client.py
+++ simopai/client.py
@@ -1,18 +1,22 @@
 from openai import OpenAI
-from exceptions import OpenAIKeyException
 
 
 class Client():
-    def __init__(self, openai_key) -> None:
-        self.client = None
+    def __init__(self, openai_key: str) -> None:
+        self.opai_client = None
 
         self._create_open_ai_client(openai_key)
 
     def _create_open_ai_client(self, key) -> None:
         if not key:
-            raise OpenAIKeyException(message="No OpenAI key provided")
+            raise Exception("No OpenAI key provided")
 
         if not isinstance(key, str):
-            raise OpenAIKeyException(message="OpenAI key must be a sting")
+            raise Exception("OpenAI key must be a sting")
 
-        self.client = OpenAI(api_key=key)
+        self.opai_client = OpenAI(api_key=key)
+        print(self.opai_client)
+
+
+if __name__ == "__main__":
+    client = Client(openai_key=123)
