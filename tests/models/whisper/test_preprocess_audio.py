60236d6004cfd932fa58127656ea3905eff69e8d
nachokhan <ignacio.rigoni@gmail.com>
2023-12-27 00:50:54 -0300

    updated test variables

--- tests/models/whisper/test_preprocess_audio.py
+++ tests/models/whisper/test_preprocess_audio.py
@@ -9,12 +9,13 @@ from simopai.models.whisper.preprocess_lib import preprocess_audio
 from simopai.models.whisper.enums import AudioProcessing, LongFileStrategy
 
 
-audio_path = "tests/models/whisper/TEST.m4a"
+TEST_AUDIO_FILE = "tests/models/whisper/audio/TEST.m4a"
+TEST_LONG_AUDIO_FILE = "tests/models/whisper/audio/LONG_TEST.m4a"
 
 
 # Test case 1: Normalization
 def test_normalize_audio():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NORMALIZE)
+    processed_paths = preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NORMALIZE)
 
     assert len(processed_paths) == 1
     assert os.path.exists(processed_paths[0])
@@ -22,7 +23,7 @@ def test_normalize_audio():
 
 # Test case 2: Noise reduction
 def test_reduce_noise():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NOISE_REDUCTION)
+    processed_paths = preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NOISE_REDUCTION)
 
     assert len(processed_paths) == 1
     assert os.path.exists(processed_paths[0])
@@ -30,7 +31,7 @@ def test_reduce_noise():
 
 # Test case 3: Splitting long audio
 def test_split_long_audio():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NORMALIZE, LongFileStrategy.SPLIT)
+    processed_paths = preprocess_audio(TEST_LONG_AUDIO_FILE, AudioProcessing.NORMALIZE, LongFileStrategy.SPLIT)
 
     assert len(processed_paths) > 1
     assert all(os.path.exists(path) for path in processed_paths)
@@ -38,7 +39,7 @@ def test_split_long_audio():
 
 # Test case 4: Windowing long audio
 def test_window_long_audio():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NORMALIZE, LongFileStrategy.WINDOW)
+    processed_paths = preprocess_audio(TEST_LONG_AUDIO_FILE, AudioProcessing.NORMALIZE, LongFileStrategy.WINDOW)
 
     assert len(processed_paths) > 1
     assert all(os.path.exists(path) for path in processed_paths)
@@ -46,7 +47,7 @@ def test_window_long_audio():
 
 # Test case 5: Default strategy with long audio
 def test_default_long_audio():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NORMALIZE)
+    processed_paths = preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NORMALIZE)
 
     assert len(processed_paths) == 1
     assert os.path.exists(processed_paths[0])
@@ -62,18 +63,18 @@ def test_invalid_audio_path():
 # Test case 7: Unsupported processing type
 def test_unsupported_processing_type():
     with pytest.raises(ValueError):
-        preprocess_audio(audio_path, 'invalid_processing_type')
+        preprocess_audio(TEST_AUDIO_FILE, 'invalid_processing_type')
 
 
 # Test case 8: Invalid long file strategy
 def test_invalid_long_file_strategy():
     with pytest.raises(ValueError):
-        preprocess_audio(audio_path, AudioProcessing.NORMALIZE, 'invalid_strategy')
+        preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NORMALIZE, 'invalid_strategy')
 
 
 # Test case 9: No processing
 def test_no_processing():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NONE)
+    processed_paths = preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NONE)
 
     assert len(processed_paths) == 1
     assert os.path.exists(processed_paths[0])
@@ -81,7 +82,7 @@ def test_no_processing():
 
 # Test case 10: No long file strategy
 def test_no_long_file_strategy():
-    processed_paths = preprocess_audio(audio_path, AudioProcessing.NORMALIZE, LongFileStrategy.DEFAULT)
+    processed_paths = preprocess_audio(TEST_AUDIO_FILE, AudioProcessing.NORMALIZE, LongFileStrategy.DEFAULT)
 
     assert len(processed_paths) == 1
     assert os.path.exists(processed_paths[0])
