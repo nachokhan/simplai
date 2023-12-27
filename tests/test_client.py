e5c86af812101daef146e43166617b99a5416f94
nachokhan <ignacio.rigoni@gmail.com>
2023-12-27 00:07:17 -0300

    added reltive import to tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 import pytest
 from simopai.client import Client
 
